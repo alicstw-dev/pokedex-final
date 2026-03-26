195436b4cf3579939b82fac89c4ca04897a7ef3d
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2026-03-26 19:57:04 -0300

    feat: ajuste da RegisterPage para se Comunicar com a API

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -2,26 +2,39 @@ import { useState } from 'react'
 import { User, Lock } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { RegisterUser } from '@/features/services/register.service.ts'
+import { useNavigate } from 'react-router'
 
 export default function RegisterPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
 
-  const handleRegister = (e: React.FormEvent) => {
+  const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault()
 
-    const userData = {
-      username,
-      password,
-    }
+    try {
+      setLoading(true)
 
-    console.log('Usuário cadastrado:', userData)
+      const response = await RegisterUser(username, password)
+
+      console.log('Usuário registrado:', response.user)
+      console.log('Token:', response.token)
+
+      navigate('/pokedex')
+
+    } catch (error) {
+      console.error('Erro ao registrar usuário', error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
     <div className="flex-1 bg-background flex mt-40 justify-center min-h-screen px-6">
       <div className="w-full max-w-md ">
-        {/* FORM */}
+        
         <form
           onSubmit={handleRegister}
           className="bg-card p-8 rounded-xl border border-border min-h-95 flex flex-col"
@@ -29,6 +42,7 @@ export default function RegisterPage() {
           <p className="text-2xl font-semibold">Criar Conta</p>
 
           <div className="space-y-6 mt-10">
+
             <div className="relative">
               <User className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground size-4" />
               <Input
@@ -50,11 +64,13 @@ export default function RegisterPage() {
               />
             </div>
 
-            <Button type="submit" className="w-full h-12">
-              Registrar
+            <Button type="submit" className="w-full h-12" disabled={loading}>
+              {loading ? 'Registrando...' : 'Registrar'}
             </Button>
+
           </div>
         </form>
+
       </div>
     </div>
   )
