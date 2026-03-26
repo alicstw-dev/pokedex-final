9469c41f7a2aa4d5ffdc9a9216214b5939d2297b
David Reis <davidsilvareis23@gmail.com>
2026-03-26 20:59:18 -0300

    feat: adicionar funcionalidade de favoritos para Pokémon

--- src/features/services/register.service.ts
+++ src/features/services/register.service.ts
@@ -1,10 +1,12 @@
-import { http } from "@/infra/http/http-client"
-import { setAccessToken } from "../storages/token.storage"
-import type { AuthResponse } from "../types/dto/auth-dto"
+import { http } from '@/infra/http/http-client'
+import { setAccessToken } from '../storages/token.storage'
+import type { AuthResponse } from '../types/dto/auth-dto'
 
-export async function RegisterUser(username: string, password: string): Promise<AuthResponse> {
-
-  const response = await http.post<AuthResponse>("register", {
+export async function RegisterUser(
+  username: string,
+  password: string,
+): Promise<AuthResponse> {
+  const response = await http.post<AuthResponse>('register', {
     username,
     password,
   })
